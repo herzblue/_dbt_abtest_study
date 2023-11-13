c1f41759bd112324572c30709eaceb20beea61c5
ygpark <ygpark@tpcg.co.kr>
2023-11-13 09:01:32 +0900

    first commit

---
+++ src/abtest/tests/dim_user_metadata.sql
@@ -0,0 +1,14 @@
+SELECT
+    *
+FROM(
+        SELECT
+            user_id,
+            COUNT(1) cnt
+        FROM
+            {{ ref("dim_user_metadata") }}
+        GROUP BY 1
+        ORDER BY 2 DESC
+        LIMIT 1
+    )
+WHERE
+    cnt > 1
